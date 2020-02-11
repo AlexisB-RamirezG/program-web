f0c2f1f314b7c981dabc2d06071396fcd51f5fb9
Your Name <you@example.com>
2020-02-11 08:51:57 -0600

    Modulos

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-ABC
+/*function greet() {
+    console.log(`Hi`);
+}*/
+
+//Nueva forma
+let greet = () => {
+    console.log(`Hi`);
+}
+
+greet();
+
+/*function logGreeting(fn) {
+    console.log(fn);
+    fn();
+}*/
+
+//Function expression   let XXXXXX = () => {}
+let logGreeting = (fn) => {
+    fn();
+}
+
+//logGreeting(greet);
+
+
+//Functions are first class so you can send them as a parameter
+//Función as parameter XXXXX(() => {});
+logGreeting(() => {
+    console.log(`Hi`);
+});
+
+//Use a function expression on the fly. See above too
+logGreeting2(function() {
+    console.log(`Hi`)
+});
