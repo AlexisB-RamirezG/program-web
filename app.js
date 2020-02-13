8885160a9886103ec06bdbfdcf74dfc0a26b3288
Your Name <you@example.com>
2020-02-13 07:54:18 -0600

    Segundo commit

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-var person = {
+let person = {
     firstname: 'John',
     lastname: 'Doe',
     greet: function() {
         console.log(`Hello, ${this.firstname} ${this.lastname}`);
     }
 }
+
+//objetc['key']   object.key
+console.log(person.firstname, person['lastname']);
+person.greet();
