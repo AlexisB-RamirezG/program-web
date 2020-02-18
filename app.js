ce612e931d962fbe7cf84d0708007a6fca3d53fe
Your Name <you@example.com>
2020-02-18 08:19:41 -0600

    Primer commit

---
+++ app.js
@@ -0,0 +1,25 @@
+// Event: Something that has happened in our app that we can respond to.
+// Inheritance: One object thatgets access to the properties and methods of another object.
+// Prototypical inheritance: 
+// Function constructors: A normal function that is used to construct objects. 
+
+function Person(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
+
+// Pointing to the class' prototype, not the instance prototype.
+Person.prototype.greet = function() {
+    console.log(`Hello, ${this.firstname} ${this.lastname}`);
+}
+
+var john = new Person('John', 'Doe');
+john.greet();
+
+var jane = new Person('Jane', 'Doe');
+jane.greet();
+
+// How to print the prototype's content
+console.log(john.__proto__);
+console.log(jane.__proto__);
+console.log(john.__proto__ === jane.__proto__);
